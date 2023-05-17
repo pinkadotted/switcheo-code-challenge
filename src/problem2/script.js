e0906e8d9e8ad668c3929bc4ac261d3a4262b66c
pinkadotted <subkmrspm@gmail.com>
2023-05-17 19:57:15 +0800

    worked on modal, still have a few bugs.

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -1,5 +1,20 @@
 const validAddressValidator = () => {
+    document.getElementById('ETHaddr').classList.remove("is-validd");
+    document.getElementById('ETHaddr').classList.remove("is-invalidd");
     const address = document.getElementById('ETHaddr').value;
+
     const isValid = ethers.utils.isAddress(address);
     console.log(isValid);   
+    // return isValid;
+    if (isValid) {
+        document.getElementById('ETHaddr').classList.add("is-validd");
+        document.getElementById('valid-div').style.visibility = "visible";
+        document.getElementById('valid-div').innerHTML = "Looks Good!"
+    }
+    else {
+        document.getElementById('ETHaddr').classList.add("is-invalidd");
+        document.getElementById('valid-div').style.visibility = "visible";
+        document.getElementById('valid-div').innerHTML = "Please enter a valid address!"
+
+    }
 }
