01a601d53d1936c9e717921f895342d7533830e5
pinkadotted <subkmrspm@gmail.com>
2023-05-17 19:57:18 +0800

    worked on modal

--- src/problem2/script.js
+++ src/problem2/script.js
@@ -1,20 +1,64 @@
 const validAddressValidator = () => {
-    document.getElementById('ETHaddr').classList.remove("is-validd");
-    document.getElementById('ETHaddr').classList.remove("is-invalidd");
-    const address = document.getElementById('ETHaddr').value;
+    let ETHaddr = document.getElementById('ETHaddr');
+    let validAddr = document.getElementById('valid-addr');
+    let address = document.getElementById('ETHaddr').value;
+    let isValid = ethers.utils.isAddress(address);
 
-    const isValid = ethers.utils.isAddress(address);
-    console.log(isValid);   
-    // return isValid;
+    ETHaddr.classList.remove("is-validd", "is-invalidd");
+
     if (isValid) {
-        document.getElementById('ETHaddr').classList.add("is-validd");
-        document.getElementById('valid-div').style.visibility = "visible";
-        document.getElementById('valid-div').innerHTML = "Looks Good!"
+        ETHaddr.classList.add("is-validd");
+        validAddr.style.visibility = "visible";
+        validAddr.innerHTML = "Looks Good!"
     }
     else {
-        document.getElementById('ETHaddr').classList.add("is-invalidd");
-        document.getElementById('valid-div').style.visibility = "visible";
-        document.getElementById('valid-div').innerHTML = "Please enter a valid address!"
-
+        ETHaddr.classList.add("is-invalidd");
+        validAddr.style.visibility = "visible";
+        validAddr.innerHTML = "Please enter a valid address!"
+        document.getElementById('submit-button').classList.add("disabled")
     }
+    return [address, isValid];
+}
+
+let ethAmt = document.getElementById('ETHamt');
+
+const isDigit = () => {
+
+    let validAmt = document.getElementById('valid-amt');
+    let amount = document.getElementById('ETHamt').value;
+    let isnum = /^[0-9\.]*$/g.test(amount);
+
+    ethAmt.classList.remove("is-validd", "is-invalidd");
+
+    if (amount.length == 0) {
+        isnum = false;
+    }
+
+    if (isnum) {
+        ethAmt.classList.add("is-validd");
+        validAmt.style.visibility = "visible";
+        validAmt.innerHTML = "Looks Good!";
+    }
+    else {
+        ethAmt.classList.add("is-invalidd");
+        validAmt.style.visibility = "visible";
+        validAmt.innerHTML = "Please enter a proper value!";
+        document.getElementById('submit-button').classList.add("disabled")
+    }
+    return [amount, isnum];
+}
+
+const bothValid = () => {
+    let [add, isval] = validAddressValidator();
+    let [amt, isnum] = isDigit();
+    if (isval && isnum) {
+        document.getElementById('submit-button').classList.remove("disabled")
+    }
+}
+
+const modalVals = () => {
+    let val = document.getElementById("modal-text");
+    let [add, isval] = validAddressValidator();
+    let [amt, isnum] = isDigit();
+    val.innerHTML = "You are sending " + amt + " ETH to: " + add + ".";
 }
